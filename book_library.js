1cd73278bd3e24b51eb972d5427b47ba8b0e32d4
Leonardo Ghiggino <lghiggino@gmail.com>
2020-09-04 20:13:28 -0300

    first commit

---
+++ book_library.js
@@ -0,0 +1,126 @@
+    console.log("Leonardo, você consegue. Focus.")
+    
+    //base library
+    let library = [
+        {title: "the hobbit",
+        author: "JRR Tolkien",
+        length: 293,
+        status: "read",
+        genre: "medieval fantasy",
+        quote: "onde upon a time there was a hobbit"   
+        },
+        {title: 1984,
+        author: "George Orwell",
+        length: 303,
+        status: "read",
+        genre: "dystopic fantasy",
+        quote: "the big brother is watching you"
+        }, 
+        {title: "Brave New World",
+        author: "Aldous Huxley",
+        length: 298,
+        status: "read",
+        genre: "dystopic fantasy",
+        quote: "If one's different, one's bound to be lonely."
+        },
+        {title: "Little mermaid",
+        author: "Walter Disney",
+        length: 130,
+        status: "read",
+        genre: "children book",
+        quote: "Somewhere into the sea"
+        },
+        {title: "Book 5 ",
+        author: "LNG",
+        length: 111,
+        status: "not read",
+        genre: "javascript programming",
+        quote: "Lets do this!"
+        },           
+         ]
+
+    //add a new book button and functionality (hoisting)
+    let addABook = document.querySelector("#addABook")
+    addABook.addEventListener("click", addBookToLibrary)
+
+    class Book{
+        constructor(title, author, length, status, genre, quote){
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.status = status;
+        this.genre = genre;
+        this.quote = quote
+        //this.coverImage = coverImage
+        }
+        //funções adicionadas que cada livro tem
+        info(){return `${this.title} by ${this.author}, ${this.length} pages, in ${this.genre} ${this.status}`} 
+        multiply(n){return this.length*n}
+        
+    }
+
+    function addBookToLibrary(event){
+        event.preventDefault()
+        //esses prompts se tornarão um form para adicionar novos livros
+        let title = prompt("what is the book title?", "");
+        let author = prompt("who is the author?", "");
+        let length = prompt("how many pages?", 0);
+        let status = prompt("have you read this book before?", "not read");
+        let genre = prompt("what is this books genre?", "");
+        let quote = prompt("would you like to add a quote?", "Lorem ipsum....");
+        //let coverImage = prompt("would you like to upload a cover image?", "Lorem ipsum....");
+
+        temporaryBookObject = new Book(title, author, length, status, genre); //adicionar cover
+        library.push(temporaryBookObject)
+
+        buildAndUpdateGrid()
+    }
+
+    function buildAndUpdateGrid(){
+        //Titulo mais recente
+        let mainBookTitle = document.querySelector("#mainBookTitle");
+        let mainBookAuthor =  document.querySelector("#mainBookAuthor");
+        let mainBookStatus =  document.querySelector("#mainBookStatus");
+        let mainBookPages =  document.querySelector("#mainBookPages");
+        
+        
+
+        mainBookTitle.textContent = library[library.length-1]["title"];
+        mainBookAuthor.textContent = library[library.length-1]["author"];
+        mainBookStatus.textContent = library[library.length-1]["status"];
+        mainBookPages.textContent = `${library[library.length-1]["length"]} pages`;
+
+        //os ultimos 4 anteriores
+        let otherBooks = document.querySelector(".otherBooks")
+        otherBooks.innerHTML = "";
+            for (i = 2; i < 6; i++){
+                let section = document.createElement("section");
+                section.classList.add("fourBooks")
+                otherBooks.appendChild(section);
+
+                let bookTitle = document.createElement("h3");
+                bookTitle.classList.add("bookTitle");
+                section.appendChild(bookTitle);
+                bookTitle.textContent = library[library.length-i]["title"];
+
+                let bookAuthor = document.createElement("span");
+                bookAuthor.classList.add("bookAuthor");
+                section.appendChild(bookAuthor);
+                bookAuthor.textContent = library[library.length-i]["author"];
+
+                let bookStatus = document.createElement("span");
+                bookStatus.classList.add("bookStatus");
+                section.appendChild(bookStatus);
+                bookStatus.textContent = library[library.length-i]["status"];
+
+                let bookPages = document.createElement("span");
+                bookPages.classList.add("bookPages");
+                section.appendChild(bookPages);
+                bookPages.textContent = `${library[library.length-i]["length"]} pages`;
+
+                let editThisBook = document.createElement("a");
+                editThisBook.classList.add("editBook");
+                section.appendChild(editThisBook);
+                editThisBook.textContent = "Edit"
+            }
+    }
---
+++ search_library.js
@@ -0,0 +1,92 @@
+console.log("search library is ON, baby");
+
+document.querySelector("#searchButton").addEventListener("click", check);
+let searchResultDisplay = document.querySelector("#searchResultDisplay");
+
+function check(){
+    let searchItem = document.querySelector("#search").value.toLowerCase();
+    let searchIndex = document.querySelector("#indexes").value;
+    let searchResultArray = [];
+    //TESTES
+    console.log(`Estamos buscando por ${searchItem}`, typeof searchItem)
+    console.log(`no índice ${searchIndex}`)
+    
+    // se o item buscado for "*"" vamos dispor todos os livros
+    // em ordem lafabetica de acordo com o INDICE marcado
+    if (searchItem === "*") return console.table(library);
+
+    //DEALING WITH BLANK SEARCH - ERROR MESSAGE
+    else if (searchItem === "") { 
+        searchResultDisplay.innerHTML = "";
+        let errorMessage = document.createElement("p");
+        errorMessage.textContent = "Please type something";
+        errorMessage.classList.add("errorMessage");
+        searchResultDisplay.appendChild(errorMessage);
+        return;
+    }        
+
+    // DEALING WITH A VALID SEARCH
+    else if (searchItem != "" && searchItem != "*"){
+        searchResultDisplay.innerHTML = "";
+        for (i = 0; i < library.length; i++){
+            if (library[i][searchIndex].toString().toLowerCase().includes(searchItem)){
+                searchResultArray.push(library[i])
+            } 
+        } console.table(searchResultArray);
+            if (searchResultArray.length>0){
+                for (i = 0; i < searchResultArray.length; i++){
+
+                let section = document.createElement("section");
+                section.classList.add("bookResults");
+                searchResultDisplay.appendChild(section)
+
+                let bookTitle = document.createElement("h4");
+                bookTitle.classList.add("bookTitle");
+                section.appendChild(bookTitle);
+                bookTitle.textContent = searchResultArray[i]["title"];
+
+                let bookAuthor = document.createElement("span");
+                bookAuthor.classList.add("bookAuthor");
+                section.appendChild(bookAuthor);
+                bookAuthor.textContent = searchResultArray[i]["author"];
+
+                let bookStatus = document.createElement("span");
+                bookStatus.classList.add("bookStatus");
+                section.appendChild(bookStatus);
+                bookStatus.textContent = searchResultArray[i]["status"];
+
+                let bookPages = document.createElement("span");
+                bookPages.classList.add("bookPages");
+                section.appendChild(bookPages);
+                bookPages.textContent = searchResultArray[i]["length"];
+
+                let editThisBook = document.createElement("a");
+                editThisBook.classList.add("editBook");
+                section.appendChild(editThisBook);
+                editThisBook.textContent = "Edit"
+                editThisBook.addEventListener("click", editThisBook)// não funciona por enquanto
+
+                }
+            }
+    }
+
+
+    
+            
+        
+    
+    
+    //criando as seções e as populando com os títulos encontrados na busca
+    
+
+ 
+    // status precisa ser repensado porque sempre terá READ ou NOT READ, então a busca precisa ser definida SEM a palavra READ
+
+    //length precisa ser pensado porque estaremos recebendo uma string de numeros no input do usuário e teremos que lidar com isso para poder calcular maior ou menor.
+
+}
+
+
+function editThisBook(){
+    console.log("hello")
+}
